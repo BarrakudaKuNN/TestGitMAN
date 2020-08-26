5e95850ddf3a6413b5246041d25ae23d10f3c5f3
BarrakudaKuNN <55402340+BarrakudaKuNN@users.noreply.github.com>
2020-08-26 16:00:58 +0300

    Добавьте файлы проекта.

---
+++ TestGitMAN/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitMAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HI im TEST 1");
+            Console.ReadLine();
+        }
+    }
+}
